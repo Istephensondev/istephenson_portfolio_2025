e7ef5f0fe7d5995e3eb6b44d5077428bd08e10aa
Istephensondev <74675624+Istephensondev@users.noreply.github.com>
2025-09-04 14:06:04 -0500

    Add files via upload

---
+++ user-data/data.js
@@ -0,0 +1,288 @@
+const data ={
+  "bio": [
+    "I’m Ivy Stephenson, a hybrid Email Marketing Specialist and Frontend Developer with over five years of experience bringing ideas to life through engaging campaigns. With a background in Salesforce Marketing Cloud, React, and e-commerce platforms, I blend technical precision with creative strategy to deliver results that are both impactful and user-friendly.",
+    "At Brookdale Senior Living, I design, segment, and deploy lifecycle programs in Salesforce Marketing Cloud—optimizing deliverability, engagement, and conversions through thoughtful A/B testing, analytics, and meticulous database management. Before that, at Universal Audio, I led front-end e-commerce development across React, Liquid, HTML/CSS/JS, and Shopify, crafting landing pages, press releases, and blog layouts with a focus on SEO, mobile performance, and page speed.",
+     "I’ve worked extensively in Agile environments, which makes me adept at translating requirements into concise tickets, leading standups, and shipping maintainable, well-tested code on tight timelines. I care deeply about component reusability, code quality, and continuous improvement—and I bring a calm, collaborative approach even under pressure."
+  ],
+  "skills": [
+    "JavaScript",
+    "TypeScript",
+    "Angular",
+    "AngularJS",
+    "React",
+    "HTML5",
+    "CSS3",
+    "Tailwind CSS",
+    "Material UI",
+    "Bootstrap",
+    "Git",
+    "GitHub Copilot",
+    "OpenAI APIs",
+    "Chrome DevTools",
+    "Performance Optimization",
+    "Responsive Design",
+    "Web Accessibility",
+ 
+
+
+  ],
+  "experience": [
+    {
+      "title": "Lifecycle Email Marketing Specialist - Brookdale Senior Living",
+      "duration": "July 2025 — Present",
+    "details": [
+         "Designing, segmenting, deploying, and managing email campaigns",
+  "Conducting A/B testing, optimizing for deliverability and conversion",
+  "Analyzing key metrics like open rate, click-through rate, conversion rate, and bounce rate",
+  "Helped manage backlog and led Agile standups and meetings",
+  "Maintaining and cleaning email lists and databases",
+  "Collaborating closely with designers, content creators, and marketing teams",
+  "Staying updated on industry best practices, compliance laws, and emerging trends",
+  "Conducted A/B testing to analyze and compare different versions of marketing campaigns or website features, measuring performance metrics such as conversion rates, engagement, and user behavior. By evaluating the results, I identified the most effective version, ensuring optimized user experiences and driving higher performance for Drip's platform.",
+  "refining campaigns that resonate with families and individuals seeking long-term care services",
+  "Integrate with Marketing Cloud (Salesforce’s email and automation platform) to send campaigns.",
+  "Segment audiences by demographics, health needs, or stage in the decision process.",
+
+      ],
+      "tags": [],
+      "icon": "money"
+    },
+   {
+      "title": "Front End Web Developer at Universal Audio, Inc.",
+      "duration": "September 2021 — October 2023",
+    
+      "details": [
+         "Worked as a front end E-Commerce Web Developer in HTML, CSS, JavaScript, Liquid, React, and Drip to maintain and update company website, creating landing pages, press releases, and blog articles",
+  "Prioritized SEO, mobile optimization, and speed in design implementation",
+  "Handled tasks and project management in Asana and Jira",
+  "Used Figma and Adobe Creative Cloud to design mockups and wireframes",
+  "Helped manage backlog and led Agile standups and meetings",
+  "Updated landing pages in Shopify with new image cards, descriptions, and embedded YouTube/website additions",
+  "Performed Quality Assurance testing",
+  "Used Drip to customize global emails and add campaigns to capture analytics and metrics, and drive revenue",
+  "Conducted A/B testing to analyze and compare different versions of marketing campaigns or website features, measuring performance metrics such as conversion rates, engagement, and user behavior. By evaluating the results, I identified the most effective version, ensuring optimized user experiences and driving higher performance for Drip's platform.",
+  "Liaised with Marketing team, Executive Leadership, Visual Designers, Copywriters, and Stakeholders to lead global email campaigns",
+  "Translated product pages, emails and press releases into French, German, and Japanese and sent globally",
+  "Diagnosed and resolved front-end website and design problems",
+  "Used Git for version control, tracking changes and team collaboration"
+      ],
+      "tags": [],
+      "icon": "money"
+    },
+    {
+      "title": "Business Analyst Internship at DaVita",
+      "duration": "April 2022 - July 2023",
+     
+      "details": [
+            "Observed and assisted full-stack developers with HTML5, CSS3, and JavaScript.",
+    "Learned Jira concepts: epics, features, stories, and tasks.",
+    "Observed and assisted business analysts in gathering business and functional requirements; wrote stories and acceptance criteria.",
+    "Attended Agile Scrum standups and meetings (kickoffs, project planning, retrospectives, demos).",
+    "Observed and assisted QA testers in regression testing and validation.",
+    "Observed and assisted developers in recreating steps for defects, refactoring, and debugging."
+      ],
+      "tags": [
+        "TypeScript",
+        "RxJS",
+        "NGXS",
+        "WebSockets",
+        "Module Federation"
+      ],
+      "icon": "heartbeat"
+    },
+   
+  ],
+  "education": [
+    {
+      "title": "Vanderbilt University Certification in Web Development",
+      "duration": "Jan 2020 — May 2021",
+      "subtitle": "Vanderbilt University - Nashville, TN",
+      "details": [
+         "Programming Languages: Python, Java, JavaScript, C++, or others.",
+  "Web Development: HTML, CSS, JavaScript, and frameworks like React, Angular, or Vue.js.",
+  "Database Management: Basics of database design, SQL (Structured Query Language), and possibly NoSQL databases.",
+  "Software Development Lifecycle: Understanding different phases of software development such as requirements gathering, design, implementation, testing, deployment, and maintenance.",
+  "Version Control Systems: Learning tools like Git for version control and collaboration.",
+  "Data Structures and Algorithms: Basics of data structures (arrays, linked lists, trees, graphs, etc.) and algorithms (sorting, searching, etc.).",
+  "Agile Methodologies: Understanding agile principles and practices commonly used in software development teams.",
+  "Debugging and Troubleshooting: Techniques for identifying and fixing bugs in code.",
+  "Object-Oriented Programming (OOP): Concepts like classes, objects, inheritance, polymorphism, encapsulation, etc.",
+  "Frameworks and Libraries: Depending on the focus of the program, students may learn about popular frameworks and libraries relevant to their area of interest, such as Django or Flask for Python web development, or Spring for Java development.",
+  "Deployment and DevOps: Basics of deploying applications and understanding DevOps principles.",
+  "Security: Fundamentals of application and data security."
+      ],
+      "tags": [],
+      "icon": "graduation-cap"
+    },
+    {
+      "title": "SAE Insitute - Audio Engineering Degree",
+      "duration": "Jan 2019 — May 2020",
+      "subtitle": "SAE Institute - Nashville, TN",
+      "details": ["Recording & Mixing: Studio/live recording, Pro Tools, Logic, Ableton",
+  "Acoustics: Sound theory, signal flow, studio setup",
+  "Electronic Music: Synthesis, MIDI, DAW-based production",
+  "Post-Production: Editing, foley, mastering for film & media",
+  "Live Sound: Concert/event sound engineering",
+  "Music Business: Marketing, royalties, entrepreneurship",
+  "Portfolio Projects: Real-world audio for music, film, games",
+  "Technical Foundations: Analog/digital systems, audio electronics"],
+      "tags": [],
+      "icon": "book"
+    }
+  ],
+  
+  "contactLinks": [
+    {
+      "label": "Email",
+      "link": "mailto:istephensondev@gmail.com",
+      "icon": "fa fa-envelope"
+    },
+    {
+      "label": "LinkedIn",
+      "link": "https://www.linkedin.com/in/ivystephenson/",
+      "icon": "fa-brands fa-linkedin"
+    },
+    {
+      "label": "GitHub",
+      "link": "https://github.com/Istephensondev",
+      "icon": "fa-brands fa-github"
+    },
+   
+  ],
+  "footer": [
+    {
+      "label": "",
+      "data": [
+       
+      ]
+    },
+    {
+      "label": "",
+      "data": [
+        {
+         
+        },
+        {
+     
+        }
+      ]
+    },
+    {
+      "label": "copyright-text",
+      "data": [
+        "Built with HTML, CSS, JavaScript, and ❤️"
+      ]
+    }
+  ]
+}
+export const projects = [
+  {
+    title: "Universal Audio Website",
+    about: "Developed and maintened Universal Audio's eCommerce website using Magento, later transitioning to Shopify for theme customization and ongoing updates.",
+    author: "Istephensondev",
+    name: "ua-website",
+    url: "https://www.uaudio.com/",
+    image: "./assets/images/uaudio.png" // 👈 add project thumbnail
+  },
+   {
+    title: "Brookdale Full Email Marketing Campaign",
+    about: "Built a custom storefront for Wholesome Goods using Shopify for backend e-commerce and Vite for a fast, modern front-end. Delivered a responsive, high-performance shopping experience.",
+    author: "Istephensondev",
+    name: "marketing-campaign",
+    url: "https://github.com/Istephensondev/email-marketing-campaign-flowchart/blob/main/chart%20(2).png",
+    image: "./assets/images/fullcampaign.png"
+  },
+  {
+    title: "Wholesome Goods Storefront",
+    about: "Built a custom storefront for Wholesome Goods using Shopify for backend e-commerce and Vite for a fast, modern front-end. Delivered a responsive, high-performance shopping experience.",
+    author: "Istephensondev",
+    name: "wholsome-goods",
+    url: "https://www.pupgrade.com/?_atid=iqwLXx0PyRIh2GWHyhUQnFwjclxXQr",
+    image: "./assets/images/wholesomegoods.png"
+  },
+   {
+    title: "Brookdale Community Email",
+    about: "Designed and coded a community email for Brookdale Senior Living, focusing on clear messaging and responsive design to engage residents and families effectively across all devices.",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/brookdale-community-email/blob/main/boardtobite3.png",
+    image: "./assets/images/boardtobite.png"
+  },
+  {
+    title: "UA Press Release",
+    about: "Designed and coded a press release for Universal Audio, combining clear, engaging copy with responsive HTML/CSS to ensure seamless performance across devices and email platforms.",
+    author: "Istephensondev",
+    name: "press-release",
+    url: "https://www.uaudio.com/blogs/press/hemisphere-mic-modeling-plug-in-and-sc-1-standard-condenser-microphone",
+    image: "./assets/images/pressrelease.png"
+  },
+  {
+    title: "Universal Audio User Story & Requirements",
+    about: "Create user stories & requirements by capturing stakeholder inputs in a Word table, drafting stories with clear requirements, Figma wireframes, and acceptance criteria; load epics/stories into Jira, refine & estimate in grooming, then schedule into the next 2-week sprint.",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/ua-requirements-userstory/blob/main/ua_requirements.png",
+    image: "./assets/images/ua_requirements.png"
+  },
+   {
+    title: "Universal Audio Marketing Email",
+    about: "Developed a responsive, globally deployed email for the Opal Synth marketing campaign, with localization, personalization, and full cross-client testing.",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/ua-opalsynth-email/blob/main/opal_synth_email_drip.png",
+    image: "./assets/images/opal_synth_istephenson.png"
+  },
+   {
+    title: "Brookdale Community Email",
+    about: "Designed and coded a community email for Brookdale Senior Living, focusing on clear messaging and responsive design to engage residents and families effectively across all devices.",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/brookdale-trunk-or-treat/blob/main/trunkortreatproof.png",
+    image: "./assets/images/trunkortreat_2.png"
+  },
+  {
+    title: "Brookdale Email Campaign Flowchart",
+    about: "This flowchart outlines the process for developing and deploying community-requested emails. The process involves two key programs: Workfront (WF) for project management and Salesforce Marketing Cloud (SFMC) for email development and deployment.",
+    author: "Istephensondev",
+    name: "email-development",
+    url: "https://github.com/Istephensondev/email-campaign-flowchart/blob/main/emailcampaignflowchart.png",
+    image: "./assets/images/flowchart.png"
+  },
+   {
+    title: "Universal Audio Global Email - French",
+    about: "I developed a globally distributed email coded in French, featuring mobile-first HTML/CSS, dynamic personalization, and region-specific compliance (GDPR/CASL/CAN-SPAM).",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/ua-oxamp-email",
+    image: "./assets/images/uaoxboxthumb.png"
+  },
+     {
+    title: "Brookdale Community Email",
+    about: "Designed and coded a community email for Brookdale Senior Living, focusing on clear messaging and responsive design to engage residents and families effectively across all devices.",
+    author: "Istephensondev",
+    name: "community-email",
+    url: "https://github.com/Istephensondev/brookdale-valencia-community-email/blob/main/valenciascreenshot.png",
+    image: "./assets/images/valenciascreenshot.png"
+  },
+   {
+    title: "Universal Audio Blog Article",
+    about: "I designed and coded a Universal Audio blog post in Shopify, building a custom Liquid article template with responsive typography, optimized images, accessibility best practices, and SEO/OG tags configurable in the theme editor.",
+    author: "Istephensondev",
+    name: "blog-article",
+    url: "https://www.uaudio.com/blogs/ua/why-apollo-sounds-better",
+    image: "./assets/images/uablogarticle.png"
+  },
+];
+
+
+export const bio = data.bio;
+
+export const skills = data.skills;
+
+export const experience = data.experience
+
+export const education = data.education
+
+export const contactLinks = data.contactLinks
+
+export const footer = data.footer;
